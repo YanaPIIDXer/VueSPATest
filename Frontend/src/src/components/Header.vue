a00804a045e1820f7e5b7491105fe4ea5e0a8103
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 10:32:36 +0900

    とりあえずヘッダ部分のComponentを作成

---
+++ Frontend/src/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bar">
+    <div>新規登録</div>
+    <div>ログイン</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.bar {
+  text-align: right;
+}
+</style>
