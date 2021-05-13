a47c2683232899fe3cee39945c7ce208ff328154
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 10:35:18 +0900

    ログインしているかどうかで表示を切り替えるようにした

--- Frontend/src/src/components/Header.vue
+++ Frontend/src/src/components/Header.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="bar">
-    <div>新規登録</div>
-    <div>ログイン</div>
+    <div v-if="isLogin">
+      <div>ログアウト</div>
+    </div>
+    <div v-else>
+      <div>新規登録</div>
+      <div>ログイン</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data: function () {
+    return {
+      isLogin: false,
+    };
+  },
 };
 </script>
 
