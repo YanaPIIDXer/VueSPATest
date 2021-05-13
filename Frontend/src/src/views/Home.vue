3e3874b301f0888c5cae529e2ec68913c41a45d8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 10:16:46 +0900

    バックエンドから受け取ったメッセージを表示するようにした

--- Frontend/src/src/views/Home.vue
+++ Frontend/src/src/views/Home.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="home">
-    <router-link to="/about">About</router-link>
-  </div>
+  <div class="home">Message:{{ message }}</div>
 </template>
 
 <script>
+const axios = require("axios").create();
 export default {
   name: "Home",
-  components: {},
+  data() {
+    return {
+      message: "",
+    };
+  },
+  mounted() {
+    this.getHello();
+  },
+  methods: {
+    getHello: function () {
+      axios
+        .get("http://localhost:3000/")
+        .then(this.onResponse)
+        .catch(this.onError);
+    },
+    onResponse: function (response) {
+      this.message = response.data.message;
+    },
+    onError: function (error) {
+      this.message = error;
+    },
+  },
 };
 </script>
