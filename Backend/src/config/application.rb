28c8b58f4bb5eb5bc05f04fc00b292a6e6e007a1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 13:46:41 +0900

    セッションが正しく動くようにした

--- Backend/src/config/application.rb
+++ Backend/src/config/application.rb
@@ -35,10 +35,11 @@ module App
     # Enable CORS.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "*"
+        origins "http://localhost"
         resource "*",
           headers: :any,
-          methods: [:get, :post, :options, :head]
+          methods: [:get, :post, :options, :head],
+          credentials: true
       end
     end
   end
