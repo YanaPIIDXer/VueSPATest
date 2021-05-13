55ce829d5b2fcfd14c9d79528ece83b9c9e05728
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 12:53:39 +0900

    登録処理を実装

--- Frontend/src/src/views/Register.vue
+++ Frontend/src/src/views/Register.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="register_form">
     <h1>新規登録</h1>
-    <form>
-      ユーザ名：<input type="text" name="name" /><br />
-      パスワード：<input type="password" name="password" /><br />
+    <form @submit.prevent="onSubmit">
+      ユーザ名：<input type="text" id="name" v-model="name" /><br />
+      パスワード：<input
+        type="password"
+        id="password"
+        v-model="password"
+      /><br />
       <input type="submit" value="登録" />
     </form>
   </div>
 </template>
 
 <script>
+const axios = require("axios").create();
+export default {
+  name: "Register",
+  data() {
+    return {
+      name: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit: function () {
+      axios
+        .post("http://localhost:3000/user/register", {
+          name: this.name,
+          password: this.password,
+        })
+        .then(function (response) {
+          if (!response.data.result) {
+            alert("登録に失敗しました。");
+            return;
+          }
+          alert("登録しました。");
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
