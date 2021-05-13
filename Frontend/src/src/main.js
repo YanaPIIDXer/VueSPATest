d4314fb40a402ffcd051b2824e66698613fdba06
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-13 13:29:07 +0900

    違うっぽいので元に戻した

--- Frontend/src/src/main.js
+++ Frontend/src/src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Axios from 'axios'
 
 Vue.config.productionTip = false
-
-Axios.defaults.withCredentials = true
 
 new Vue({
   router,
